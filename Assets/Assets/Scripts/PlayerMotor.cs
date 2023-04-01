1b08ea12b9ccf6165861431becf044c402e59001
Ulaş Gergerli <20200808049@ogr.akdeniz.edu.tr>
2023-04-01 17:54:53 +0300

    animations

--- Assets/Assets/Scripts/PlayerMotor.cs
+++ Assets/Assets/Scripts/PlayerMotor.cs
@@ -4,41 +4,83 @@ using UnityEngine;
 
 public class PlayerMotor : MonoBehaviour
 {
-    private bool running;
+    private State currentState;
     private int combo;
     private Animator anim;
+
+    public enum State
+    {
+        Idle,
+        Crouching,
+        Running,
+        Walking,
+        Attacking
+    }
 
     private float cdTimer;
 
     private void Awake()
     {
+        currentState = State.Idle;
         anim = GetComponent<Animator>();
     }
 
 
     private void Update()
     {
+        if (currentState == State.Running)
+        {
+            transform.position += Vector3.forward * Time.deltaTime * 2f;
+            anim.Play("RunSword");
+        }
+        else if (currentState == State.Walking)
+        {
+            transform.position += Vector3.forward * Time.deltaTime * 0.8f;
+            anim.Play("WalkSword");
+        }
+        else if (currentState == State.Crouching)
+            anim.Play("CrouchIdle");
+        else if (currentState == State.Idle && currentState != State.Attacking)
+            anim.Play("Idle");
+
         cdTimer += Time.deltaTime;
 
-        if(Input.anyKeyDown)
+        if (Input.anyKeyDown)
             KeyInputHandler();
+        else if (Input.GetKey(KeyCode.LeftControl))
+            ControlsInputHandler();
+        else
+            currentState = State.Idle;
 
         if (cdTimer < 1f)
             return;
 
-        if(Input.GetMouseButtonDown(0))
+        AttackInputHandler();
+    }
+
+    private void AttackInputHandler()
+    {
+        if (Input.GetMouseButtonDown(0))
         {
-            if (anim.GetCurrentAnimatorStateInfo(0).IsName("Slash0"))
+            if (currentState == State.Crouching)
+                anim.Play("CrouchSlash");
+            else if (anim.GetCurrentAnimatorStateInfo(0).IsName("Slash0"))
                 anim.Play("Slash1");
             else
                 anim.Play("Slash0");
-
+            currentState = State.Attacking;
             cdTimer = 0;
-        } 
-        else if(Input.GetMouseButtonDown(1))
-        {
-            
         }
+        else if (Input.GetMouseButtonDown(1))
+        {
+            Debug.Log("Left Click");
+        }
+    }
+
+
+    private void ControlsInputHandler()
+    {
+        currentState = State.Crouching;
     }
 
     private void KeyInputHandler()
@@ -48,6 +90,10 @@ public class PlayerMotor : MonoBehaviour
         switch(input)
         {
             case "w":
+                if (Input.GetKey(KeyCode.LeftShift))
+                    anim.Play("RunSword");
+                else
+                    anim.Play("WalkSword");
                 break;
             case " ":
                 break;
